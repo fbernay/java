269efd93bfe3eaaaea80498821ff721e59f42612
fbernay <facundo.bernay1245@gmail.com.ar>
2022-01-04 16:22:32 -0300

    clase_06 creada

---
+++ clase_06/src/clase_06/Clase_05.java
@@ -0,0 +1,16 @@
+package clase_06;
+
+public class Clase_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Clase 06");
+		
+		
+		
+		
+		
+	}
+
+}
